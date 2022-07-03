d588eb4fd48ef8a7fea34fc92d7e43c526671c50
triton0305 <99065149+triton0305@users.noreply.github.com>
2022-07-03 21:02:01 +0900

    ~CreateBoardDto
    CreateBoardDto

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Injectable()
+export class BoardsService {
+
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto) {
+       
+        const {title, description } = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
+    }
+}
